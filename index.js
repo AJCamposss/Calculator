e9f7c88a607e6db0c4028e08e6419ab0b51df72d
AJCamposss <adrian.campos869@gmail.com>
2023-10-23 11:19:00 -0700

    Completed simpleCalculator

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 window.onload = init;
 
-function init() {
+function init() 
+{
+    document.getElementById("addBtn").onclick = add;
 
-    const addBtn = doccument.getElementById("addBtn")
-    addBtn.onclick = onAddButtonClicked;
+    document.getElementById("subtractBtn").onclick = subtract;
+
+    document.getElementById("multiplyBtn").onclick = multiply;
+
+    document.getElementById("divideBtn").onclick = divide;
 }
 
-function onAddButtonClicked() {
-    const numberInput = document.getElementById(+"number1Field");
-    const numberInput2 = document.getElementById(+"number2Field");
-    let addTheNumbers = userInput1 + UserInput2
-    alert(numbersAdded)
+function add()
+{
+    const number1 = parseFloat(document.getElementById("number1Field").value);
+    const number2 = parseFloat(document.getElementById("number2Field").value);
+
+    const answer = number1 + number2;
+
+    document.getElementById("answerField").value = answer;
 }
+
+function subtract()
+{
+    const number1 = parseFloat(document.getElementById("number1Field").value);
+    const number2 = parseFloat(document.getElementById("number2Field").value);
+    
+    const answer = number1 - number2;
+
+    document.getElementById("answerField").value = answer;
+}
+
+function multiply() 
+{
+    const number1 = parseFloat(document.getElementById("number1Field").value);
+    const number2 = parseFloat(document.getElementById("number2Field").value);
+
+    const answer = number1 * number2
+
+    document.getElementById("answerField").value = answer
+}
+
+function divide()
+{
+    const number1 = parseFloat(document.getElementById("number1Field").value);
+    const number2 = parseFloat(document.getElementById("number2Field").value);
+
+    const answer = number1 / number2;
+
+    document.getElementById("answerField").value = answer;
+}
+    
+
